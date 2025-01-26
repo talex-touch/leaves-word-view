a8a262530c00d5e0a5c09d02bda027d9f89c7c8a
TalexDreamSoul <2418876761@qq.com>
2025-01-26 16:13:20 +0800

    refactor(config): 修改出口路径和压缩配置
    - 更新 exportStatic 配置，增加额外路由路径
    - 添加 esbuildMinifyIIFE 配置项
    - 修改项目名称和版本号

--- config/config.ts
+++ config/config.ts
@@ -151,7 +151,10 @@ export default defineConfig({
     // 检测 className 的文件范围，若项目不包含 src 目录，可使用 `pages/**/*.tsx`
     watch: ['src/**/*.tsx']
   },
-  publicPath: './'
+  exportStatic: {
+    extraRoutePaths: ['/']
+  },
+  esbuildMinifyIIFE: true
   // plugins: [
   //   new MonacoWebpackPlugin({
   //     languages: ['javascript', 'typescript', 'css', 'html', 'json'],
