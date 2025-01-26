3f7ac4e7189212eba69fba7432549e420e80a346
TalexDreamSoul <2418876761@qq.com>
2025-01-26 16:46:02 +0800

    fix(constants): 更新线上后端主机地址
    - 将 BACKEND_HOST_PROD 从 "https://quotawish.com" 修改为 "https://api.leavesword.quotawish.com"
    - 此更新确保应用程序连接到正确的后端服务

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -6,6 +6,6 @@ export const BACKEND_HOST_LOCAL = "http://localhost:8101";
 /**
  * 线上后端地址
  */
-export const BACKEND_HOST_PROD = "https://quotawish.com";
+export const BACKEND_HOST_PROD = "https://api.leavesword.quotawish.com";
 
 export const REMOTE_BASE_AUDIO = "https://dict.youdao.com/dictvoice"
